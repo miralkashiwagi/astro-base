cc34e9380764d0d51838c70df2ca372912e4f1fa
miral_kashiwagi <miral@infoseek.jp>
2024-02-03 21:05:45 +0900

    [add] utiles

---
+++ src/js/utiles/utiles.js
@@ -0,0 +1,65 @@
+/**
+ * 引数で指定されたDOM要素(element)に対する特定のイベント(event)のハンドラ(handler)を割り当てる
+ * 割り当てたイベントリスナーを後で解除するためのAPI（unsubscribeメソッド）を持つオブジェクトを返す
+ */
+const attachEvent = (element, event, handler, options) => {
+  // イベントリスナーを要素に割り当て
+  element.addEventListener(event, handler, options);
+
+  // イベントの割り当てを解除するためのAPIを返す
+  return {
+    unsubscribe() {
+      element.removeEventListener(event, handler);
+    }
+  };
+};
+export { attachEvent }
+
+
+/**
+ * モーダルウィンドウやポップアップを前面に表示する際にページの背景を固定する
+ */
+const backfaceFixed = (fixed) => {
+  /**
+   * 表示されているスクロールバーとの差分を計測し、背面固定時はその差分body要素に余白を生成する
+   */
+  const scrollbarWidth = window.innerWidth - document.body.clientWidth;
+  document.body.style.paddingRight = fixed ? `${scrollbarWidth}px` : '';
+
+  /**
+   * 背面固定する対象を決定する
+   */
+  const scrollingElement =
+    'scrollingElement' in document
+      ? document.scrollingElement
+      : document.documentElement;
+
+  /**
+   * 背面固定時に変数にスクロール量を格納
+   */
+  const scrollY = fixed
+    ? scrollingElement.scrollTop
+    : parseInt(scrollingElement.style.top || '0');
+
+  /**
+   * CSSで背面を固定
+   */
+  const styles = {
+    height: '100vh',
+    left: '0',
+    overflow: 'hidden',
+    position: 'fixed',
+    top: `${scrollY * -1}px`,
+    width: '100vw'
+  };
+
+  Object.keys(styles).forEach((key) => {
+    scrollingElement.style[key] = fixed ? styles[key] : '';
+  });
+
+  /**
+   * 背面固定解除時に元の位置にスクロールする
+   */
+  if (!fixed) window.scrollTo(0, scrollY * -1);
+};
+export { backfaceFixed }
